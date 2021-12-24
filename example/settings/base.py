6dad3d5c013b8c1e1c2df276cea4c43d629e62e4
realnoobs <sasri.darmajaya@gmail.com>
2021-12-25 04:13:06 +0700

    add caching to wagtail

--- example/settings/base.py
+++ example/settings/base.py
@@ -34,6 +34,7 @@ INSTALLED_APPS = [
     "modelcluster",
     "taggit",
     "storages",
+    "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
     "django.contrib.sessions",

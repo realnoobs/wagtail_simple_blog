3c6462aa7a550b18a2c2d1463f1d7a38456164a4
realnoobs <sasri.darmajaya@gmail.com>
2021-12-24 22:18:15 +0700

    add apps

--- example/settings/base.py
+++ example/settings/base.py
@@ -131,9 +131,7 @@ STATICFILES_FINDERS = [
     "django.contrib.staticfiles.finders.AppDirectoriesFinder",
 ]
 
-STATICFILES_DIRS = [
-    os.path.join(PROJECT_DIR, "static"),
-]
+STATICFILES_DIRS = []
 
 # ManifestStaticFilesStorage is recommended in production, to prevent outdated
 # JavaScript / CSS assets being served from cache (e.g. after a Wagtail upgrade).
